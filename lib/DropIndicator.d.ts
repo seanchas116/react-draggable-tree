469c05773d714d3ece055e11f0e2c3fbf5e008ec
Ryohei Ikegami <iofg2100@gmail.com>
2018-02-10 15:17:05 +0900

    Build

---
+++ lib/DropIndicator.d.ts
@@ -0,0 +1,16 @@
+import React = require("react");
+export interface DropIndicatorProps {
+    rowHeight: number;
+    indent: number;
+    dropOverClassName?: string;
+    dropBetweenClassName?: string;
+}
+export interface DropIndicatorState {
+    type: "none" | "over" | "between";
+    index: number;
+    depth: number;
+}
+export declare class DropIndicator extends React.Component<DropIndicatorProps, DropIndicatorState> {
+    state: DropIndicatorState;
+    render(): JSX.Element;
+}
