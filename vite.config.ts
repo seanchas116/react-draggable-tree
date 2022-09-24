49e96c10b988ff9b0bac2c228ded26d16f8f4cf3
Ryohei Ikegami <iofg2100@gmail.com>
2022-09-24 19:41:07 +0900

    Emit .d.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,5 +15,5 @@ export default defineConfig({
       external: ["react", "react-dom"],
     },
   },
-  plugins: [react()],
+  plugins: [react(), dts()],
 });
